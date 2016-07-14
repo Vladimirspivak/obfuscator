dd72319ed1f43787c9c589238e6536533fa8b201
Vladimir Spivak <ak1010bh@gmail.com>
2016-07-14 19:03:34 +0300

    version 1.1

--- obfuscator-module.js
+++ obfuscator-module.js
@@ -6,8 +6,16 @@ module.exports = function(data){
     var obj = {};
     for (var i = 0; i < data.length; i++) {
         var item = data[i];
-        if(obj[item]) continue;
-        obj[item]= item + '_' + i;
+        if(obj[item]){
+            obj[item]= obj[item].substring(0,obj[item].indexOf('_'));
+            if (obj[item].length > 1) {
+                obj[item] = obj[item].slice(0,-1) + '_' + i;
+            } else {
+                obj[item] = obj[item] + '_' + i;
+            }
+        } else {
+            obj[item]= item + '_' + i;
+        }
     }
     return obj;
 }
